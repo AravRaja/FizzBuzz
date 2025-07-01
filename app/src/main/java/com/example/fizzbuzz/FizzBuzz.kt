af49eb1a64b7eebf32f2d03c82f68bfbc7ff05f8
Arav Raja <Arav.Raja@softwire.com>
2025-07-01 13:59:26 +0100

    Finished Initial FizzBuzz Challenge

--- app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
@@ -1,5 +1,11 @@
 package com.example.fizzbuzz
 
 fun main() {
-    print("Hello World")
+    for (i in 1..100){
+        if (i % 15 == 0)  print("FizzBuzz")
+        else if (i % 5 == 0)  print("Buzz")
+        else if (i % 3 == 0)  print("Fizz")
+        else print(i)
+
+    }
 }
