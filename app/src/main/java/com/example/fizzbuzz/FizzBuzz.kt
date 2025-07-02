ee9ee49ce03cf38d32d1a24c5566b38ad0920ab4
Arav Raja <Arav.Raja@softwire.com>
2025-07-02 14:53:39 +0100

    Cleaned up code and made fixes according to PR

--- app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
+++ app/src/main/java/com/example/fizzbuzz/FizzBuzz.kt
@@ -1,40 +1,36 @@
 package com.example.fizzbuzz
 
-//Function that takes the output res and adds Fezz in the correct place before any B-word
-fun addFezz(res: MutableList<String> ) {
-    var isFezzAdded = false
-    var count = 0
-    for (i in res){
 
-        if (i != "Fizz"){
-            res.add(count, "Fezz")
-            isFezzAdded = true
-            break
+fun addFezzBeforeWordsStartingWithB(wordList: MutableList<String> ) {
+    var count = 0
+    for (i in wordList){
+
+        if (i.startsWith('B')){
+            wordList.add(count, "Fezz")
+            return
         }
         count += 1
     }
-    if (!isFezzAdded) {
-        res.add("Fezz")
-    }
+
+    wordList.add("Fezz")
+
 }
 
 fun numClassifier(i: Int, rules: MutableList<Int>): String{
     if (0 in rules) return i.toString()
 
-    val res = mutableListOf<String>() // res represent the result of any certain number
-    //constructs res which will be the output depending on the conditions
-    if (i%3 ==0 && (3 in rules)) res.add("Fizz")
-    if (i%5 ==0 && (5 in rules)) res.add("Buzz")
-    if (i%7 == 0 && (7 in rules)) res.add("Bang")
-    if (i%11 == 0 && (11 in rules)) {
-        res.removeAll(res)
-        res.add("Bong")
+    val numberToWord = mutableListOf<String>()
+    if (i%11 == 0 && (11 in rules)) numberToWord.add("Bong")
+    else {
+        if (i % 3 == 0 && (3 in rules)) numberToWord.add("Fizz")
+        if (i % 5 == 0 && (5 in rules)) numberToWord.add("Buzz")
+        if (i % 7 == 0 && (7 in rules)) numberToWord.add("Bang")
     }
-    if (i%13 == 0 && (13 in rules)) addFezz(res)
-    if (i%17 ==0 && (17 in rules)) res.reverse()
-    if (res.isEmpty()) res.add(i.toString())
+    if (i%13 == 0 && (13 in rules)) addFezzBeforeWordsStartingWithB(numberToWord)
+    if (i%17 ==0 && (17 in rules)) numberToWord.reverse()
+    if (numberToWord.isEmpty()) numberToWord.add(i.toString())
 
-    return (res.joinToString(separator = ""))
+    return (numberToWord.joinToString(separator = ""))
 }
 fun main() {
 
@@ -53,9 +49,8 @@ fun main() {
     var maxNum = 0
     println("Enter an integer greater than or equal to 1 to be the maximum number in your fizzbuzz sequence")
     while (maxNum == 0) {
-        val input: String? = readlnOrNull()
         try {
-            maxNum = input!!.toInt()
+            maxNum = readln().toInt()
         } catch (e: NumberFormatException) {
             println("Please enter only valid integers greater than or equal 1, make sure you only use digits and no floats!")
         }
@@ -81,7 +76,7 @@ fun main() {
 
 
             } catch (e: Exception) {
-                println("Make sure your input is valid and only the numbers [3, 5, 7, 11, 13, 17] separated by commas with no spaces")
+                println("Make sure your input is valid and you only use the numbers [3, 5, 7, 11, 13, 17] separated by commas")
                 println("PLEASE TRY AGAIN:")
                 rules.removeAll(rules)
             }
